daed01170db02105e6c2fd5ae2a80c12c377db85
iiblacksmithii <94884566+iiiblacksmithiii@users.noreply.github.com>
2026-08-16 23:21:41 -0600

    Add words API try again

--- functions/api/words.js
+++ functions/api/words.js
@@ -1,5 +1,25 @@
 export async function onRequestGet(context) {
-  const { results } = await context.env.DB.prepare("SELECT * FROM words").all();
+  try {
+    if (!context.env.DB) {
+      return Response.json({ error: "DB binding is missing" }, { status: 500 });
+    }
 
-  return Response.json(results);
+    const { results } = await context.env.DB.prepare(
+      "SELECT * FROM words",
+    ).all();
+
+    return Response.json(results);
+  } catch (error) {
+    return Response.json(
+      {
+        error: error.message,
+      },
+      { status: 500 },
+    );
+  }
 }
+// export async function onRequestGet(context) {
+//   const { results } = await context.env.DB.prepare("SELECT * FROM words").all();
+
+//   return Response.json(results);
+// }
