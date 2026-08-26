530906df186e8f461367ce98c159c8f51bc597c8
iiblacksmithii <94884566+iiiblacksmithiii@users.noreply.github.com>
2026-08-26 14:44:54 -0600

    add header/footer

--- js/components.js
+++ js/components.js
@@ -8,7 +8,7 @@ const header = `
       <a class="site-brand" href="/">
         <img
             class="brand-logo"
-            src="/images/Logo.png"
+            src="/images/Logo.jpg"
             alt=""
         >
         <span class="brand-name">AUGMENT DESIGN</span>
